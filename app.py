7c37377bfbf58210cd7b7b86719107b73b819d50
Khush06k <khushkalamkar@gmail.com>
2025-09-26 20:52:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -138,9 +138,9 @@ def get_sentiment(review):
             compound += adj_score
 
     # ------------------- Final Sentiment -------------------
-    if compound >= 0.05:
+    if compound >= 0.10:
         return "Positive", compound, cleaned_review
-    elif compound <= -0.05:
+    elif compound <= -0.10:
         return "Negative", compound, cleaned_review
     else:
         return "Neutral", compound, cleaned_review
